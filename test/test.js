9cbb4befb51c86534d8e9d146d8ccece0533e725
Dietz, Michael <Michael.Dietz@nordicsemi.no>
2016-06-17 16:31:34 +0200

    Fix test framework.

--- test/test.js
+++ test/test.js
@@ -4,14 +4,25 @@ var expect = require('chai').expect;
 
 var index = require('../index');
 
+function check( done, f ) {
+  try {
+    f();
+    done();
+  } catch( e ) {
+    done( e );
+  }
+}
+
 describe('#echo', function() {
-    it('Prompts slave to echo one byte back to host', function() {
+    it('prompts slave to echo one byte back to host', function(done) {
         var buffer = '\x01';
         var len = '\x01';
         var result = index.echo(buffer, len);
-        index.port.on('data', data => {
+        expect(result).to.equal(true);
+        index.port.on('data', function (data) {
           console.log('Data: ' + data);
-          expect(result).to.equal(true);
+          expect(data).to.equal(buffer);
+          done()
         });
     });
 });
