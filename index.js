9c78a96a0dad2f79393ea62f39aac5701253ec51
Dietz, Michael <Michael.Dietz@nordicsemi.no>
2016-06-17 16:07:33 +0200

    Added echo function and unit test.

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
 'use strict';
 
-/**
- * Adds commas to a number
- * @param {number} number
- * @param {string} locale
- * @return {string}
+var SerialPort = require('serialport');
+
+/*SerialPort.list(function (err, ports) {
+  ports.forEach(function(port) {
+    console.log(port.comName);
+    console.log(port.pnpId);
+    console.log(port.manufacturer);
+  });
+});*/
+
+var port = new SerialPort.SerialPort('COM44', {
+  baudRate: 115200,
+  rtscts: true
+});
+
+port.on('error', function(err) {
+  console.log('Error: ', err.message);
+})
+
+exports.port = port
+
+/** @brief executes an echo-test
+ *  @details
+ *  Prompts the slave to echo whatever the buffer contains
+ *  @param buffer memory containing data to echo
+ *  @param len amount of data to send
+ *  @return True if the data was successfully queued for sending,
+ *  false if there is no more space to store messages to send.
  */
-module.exports = function(number, locale) {
-    return number.toLocaleString(locale);
-};
+exports.echo = function(buffer, len) {
+  port.write(len + '\x02' + buffer, function(err) {
+    if (err) {
+      console.log('Error when performing echo: ', err.message);
+      return false;
+    }
+    console.log('echo command sent');
+  });
+  return true;
+}
--- test/test.js
+++ test/test.js
@@ -1,46 +1,17 @@
 'use strict';
 
 var expect = require('chai').expect;
-var numFormatter = require('../index');
 
-describe('#numFormatter', function() {
-    it('should convert single digits', function() {
-        var result = numFormatter(1);
-        expect(result).to.equal('1');
-    });
+var index = require('../index');
 
-    it('should convert double digits', function() {
-        var result = numFormatter(12);
-        expect(result).to.equal('12');
-    });
-
-    it('should convert triple digits', function() {
-        var result = numFormatter(123);
-        expect(result).to.equal('123');
-    });
-
-    it('should convert 4 digits', function() {
-        var result = numFormatter(1234);
-        expect(result).to.equal('1,234');
-    });
-
-    it('should convert 5 digits', function() {
-        var result = numFormatter(12345);
-        expect(result).to.equal('12,345');
-    });
-
-    it('should convert 6 digits', function() {
-        var result = numFormatter(123456);
-        expect(result).to.equal('123,456');
-    });
-
-    it('should convert 7 digits', function() {
-        var result = numFormatter(1234567);
-        expect(result).to.equal('1,234,567');
-    });
-
-    it('should convert 8 digits', function() {
-        var result = numFormatter(12345678);
-        expect(result).to.equal('12,345,678');
+describe('#echo', function() {
+    it('Prompts slave to echo one byte back to host', function() {
+        var buffer = '\x01';
+        var len = '\x01';
+        var result = index.echo(buffer, len);
+        index.port.on('data', data => {
+          console.log('Data: ' + data);
+          expect(result).to.equal(true);
+        });
     });
 });
